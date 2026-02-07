b3ae6468c5a4be2613ade6265b2d8c0582357a5b
thomashamilton <thomas.hamil2011@gmail.com>
2026-02-07 22:53:16 +0000

    Bro Code java lesson 6 Shopping Cart Challenge completed.

---
+++ src/Bro/Six/Main.java
@@ -0,0 +1,28 @@
+package Bro.Six;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+String itemName;
+double price;
+int quantity;
+double total;
+
+        System.out.print("What item would you like to buy?: ");
+        itemName = s.nextLine();
+        s.reset();
+        System.out.print("\nWhat is the price for each item?: ");
+        price = s.nextDouble();
+        s.reset();
+        System.out.print("\nHow many do you want to have?: ");
+        quantity = s.nextInt();
+
+        total = price * (double) quantity;
+
+        System.out.println("\nYou have bought " + quantity + " " + itemName + "'s");
+        System.out.println("Your total cost is: $" + total);
+    }
+}
