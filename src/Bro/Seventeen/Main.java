412a2d89209d67267c6647087dd079974d4ba090
thomashamilton <thomas.hamil2011@gmail.com>
2026-02-23 20:37:24 +0000

    Bro Code java lesson 17 Temperature conversion app using Scanner, If hasNextLine and hasNextDouble and the Ternary function.

---
+++ src/Bro/Seventeen/Main.java
@@ -0,0 +1,66 @@
+package Bro.Seventeen;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //TEMPERATURE CONVERSION APP.
+
+        Scanner s = new Scanner(System.in);
+
+        double temp = 0.00d;
+        double newTemp;
+        String unit = "";
+        boolean flag = true;
+
+        System.out.print("Welcome to Toms Temperature converter.\nEnter the temperature you wish to convert as a number only:  ");
+
+        while (flag) {
+
+            if (s.hasNextDouble()) {
+                temp = s.nextDouble();
+                s.nextLine();
+                flag = false;
+            } else {
+                System.out.println("You didn't enter a double i.e 16.65 or 22.10 please enter a double number only.");
+                s.next();
+            }
+
+        }
+
+        flag = true;
+        System.out.print("Now which temperature is it in C for celcius or F for farhenheit: ");
+
+        while (flag) {
+
+            if (s.hasNextLine()) {
+                unit = s.nextLine();
+
+                if (!unit.isEmpty()) {
+                    unit = unit.toUpperCase();
+                    unit = unit.substring(0, 1);
+                    if (unit.contains("C") || unit.contains("F")) {
+                        flag = false;
+                    } else {
+                        System.out.print("\nYou didn't enter a C or a F:");
+
+                    }
+
+                }
+
+            }
+
+        }
+        newTemp = (unit.contains("C"))? (temp * 1.8) + 32 : (temp - 32) / 1.8;
+
+        System.out.printf("\n%.2f°%S converted to ", temp,unit );
+        System.out.print((unit.contains("C"))? "Fahrenheit ":"Celsius ");
+        System.out.printf("is %.2f",newTemp);
+        System.out.println((unit.contains("C"))? "°F ":"°C ");
+
+        s.close();
+}
+
+
+}
